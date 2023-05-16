85170c072224a66d841ab95817433eb85f95a16e
Becky <bjentwistle@gmail.com>
2023-05-16 15:16:08 +0100

    initial commit of homework folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './cakes.jpeg'; // Tell webpack this JS file uses this image
+
+console.log(logo); //
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1> My Baking To Do List </h1>
+      <img src={logo} alt="Cakes background"/>;
     </div>
+
   );
 }
 
